b03dbbbc75e5ae3ecad650a3f50ee81d30b02f47
eslam97 <e01095097908@gmail.com>
2023-11-24 14:00:28 +0200

    dashboard

--- src/modules/dashboard/pages/index.vue
+++ src/modules/dashboard/pages/index.vue
@@ -4,47 +4,47 @@
         <p class="text-2xl mb-2">مبيعات</p>
         <h3 class="mb-0">{{ stats.sales }}</h3>
       </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'products'})">
       <p class="text-2xl mb-2">عدد المنتجات</p>
       <h3 class="mb-0">{{ stats.total_products }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'orders'})">
       <p class="text-2xl mb-2">عدد الطلبات</p>
       <h3 class="mb-0">{{ stats.total_orders }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'customers'})">
       <p class="text-2xl mb-2">عدد البائعيين</p>
       <h3 class="mb-0">{{ stats.total_customers }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4"  @click="$router.push({name: 'orders', query: {type: 'pending_order_count'}})">
       <p class="text-2xl mb-2">عدد الطلبات المعلقة</p>
       <h3 class="mb-0">{{ stats.pending_order_count }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'orders', query: {type: 'processing_order_count'}})">
       <p class="text-2xl mb-2">عدد الطلبات جارى تحضيرها</p>
       <h3 class="mb-0">{{ stats.processing_order_count }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4"  @click="$router.push({name: 'returnedOrder'})">
       <p class="text-2xl mb-2">عدد الطلبات الملغية</p>
       <h3 class="mb-0">{{ stats.cancelled_order_count }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'orders', query: {type: 'confirmed_order_count'}})">
       <p class="text-2xl mb-2">عدد الطلبات المستلمة</p>
       <h3 class="mb-0">{{ stats.confirmed_order_count }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'orders', query: {type: 'confirmed_order_count'}})">
       <p class="text-2xl mb-2">عدد الطلبات التى تم الموافقة عليها</p>
       <h3 class="mb-0">{{ stats.confirmed_order_count }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'orders', query: {type: 'shipping_order_count'}})">
       <p class="text-2xl mb-2">عدد طلبات الشحن</p>
       <h3 class="mb-0">{{ stats.shipping_order_count }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'orders', query: {type: 'rejected_order_count'}})">
       <p class="text-2xl mb-2">عدد الطلبات المرفوضة</p>
       <h3 class="mb-0">{{ stats.rejected_order_count }}</h3>
     </div>
-    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4">
+    <div class="flex flex-col bg-gray-100 border border-primary-300 rounded p-4" @click="$router.push({name: 'orders', query: {type: 'delivered_order_count'}})">
       <p class="text-2xl mb-2">عدد الطلبات التي تم تسليمها</p>
       <h3 class="mb-0">{{ stats.delivered_order_count }}</h3>
     </div>
