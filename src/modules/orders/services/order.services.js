c7ee30ab8e9d6886bd40e8369557083d6119296c
Islam Ashraf <eslamjob97@gmail.com>
2023-12-05 11:49:34 +0200

    notification

--- src/modules/orders/services/order.services.js
+++ src/modules/orders/services/order.services.js
@@ -1,6 +1,6 @@
 import { api } from '@/axios';
 export default {
     changeStatus (id, payload) {
-        return api().patch(`admin/return-orders/${id}`, payload)
+        return api().patch(`admin/orders/${id}`, payload)
     }
 }
