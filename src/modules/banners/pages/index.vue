40a1e5cbfba6bf2a83d128cf5c3332c302afbe11
Ahmed Amin <ahmedamin@AR-MacBook-Pro.local>
2024-03-16 17:20:16 +0200

    fixes

--- src/modules/banners/pages/index.vue
+++ src/modules/banners/pages/index.vue
@@ -1,45 +1,135 @@
 <template>
   <Box class="p-5">
-    <div class="flex justify-between items-center mb-10 border-b-2 pb-3">
-      <h5 class="text-md mb-4">اللافتات</h5>
-    </div>
-    <div class="flex gap-4">
-      <div  v-for="(item, key) in allBanners" class="relative w-100 md:w-[216px] !h-[132px] w-100 rounded-xl border-2 border-mutedColor border-dashed bg-center	 flex-col bg-contain bg-no-repeat	" :style="{ backgroundImage: 'url(' + item.imageUrl + ')' }">
-        <div class="delete_box shadow cursor-pointer" @click="deleteImage(item.id)">
-          <i class="pi pi-trash text-white" />
-        </div>
+    <Dialog
+      v-model:visible="visible"
+      maximizable
+      modal
+      :header="edit ? 'تعديل' : 'إضافة'"
+      :style="{ width: '50vw' }"
+      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+    >
+      <div>
+        <ValidationForm @submit="onsubmit" :initialValues="details" v-slot="{ values }">
+          <InputField
+            type="text"
+            validation="required"
+            placeholder="اللينك"
+            name="path"
+          />
+          <MainSelect
+            :options="countries"
+            name="country_id"
+            optionLabel="name"
+            optionValue="id"
+            validation="required"
+            placeholder="اختر البلد"
+          />
+          <Upload
+            name="imageUrl"
+            label="رفع صورة"
+            @upload="uploadImage"
+            :imageValue="url"
+          />
+          <div class="sign-up__button-action mt-4">
+            <AppButton
+              class=""
+              type="submit"
+              :loading="loading"
+              submit-title="حفظ "
+            ></AppButton>
+          </div>
+        </ValidationForm>
       </div>
-      <Upload name="new" label="رفع صورة جديدة" @upload="uploadImage" resetAfterLoad/>
+    </Dialog>
+    <div class="flex justify-between items-center border-b-2 pb-3 mb-4">
+      <h5 class="text-md mb-4">التواصل الاجتماعى</h5>
+      <app-button
+        v-if="$hasPer('products:create')"
+        submit-title="إضف جديد"
+        class="rounded-lg font-medium !py-3"
+        @click="
+          visible = true;
+          details = {};
+          edit = false;
+        "
+      >
+        <template v-slot:icon> + </template>
+      </app-button>
     </div>
+    <main-table
+      actionDots
+      :list_url="'admin/banner'"
+      :loadingTable="loadingTable"
+      :actions="actions"
+      :filters="filter"
+      :columns="columns"
+    >
+      <template v-slot:imageUrl="{ data }">
+        <Avatar class="image-url" :image="data.imageUrl" :alt="data.imageUrl" />
+      </template>
+      <template v-slot:country_id="{ data }">
+        <span>{{ data?.country?.name ?? 'عامه' }}</span>
+      </template>
+    </main-table>
   </Box>
 </template>
 
 <script setup>
-import Upload from "@/components/global/formElements/Upload.vue"
-
-import bannerService from '../services/banner.services'
 import { ref } from "vue";
+import Avatar from "primevue/avatar";
+import Upload from "@/components/global/formElements/Upload.vue";
+import bannerServices from "../services/banner.services";
+import { useCountries } from "../../country/composables";
+const { countries } = useCountries();
 
-const allBanners = ref([])
-const loading = ref(false)
-function getAllBanners () {
-  loading.value = true
-  bannerService.getAllBanner().then(res => {
-    allBanners.value = res.data.data
-  }).finally(() => {
-    loading.value = false
-  })
+const loadingTable = ref(false);
+const columns = [
+  {
+    field: "id",
+    header: "#",
+  },
+  {
+    field: "imageUrl",
+    header: "الصورة",
+  },
+  {
+    field: "path",
+    header: "الرابط",
+  },
+  {
+    field: "country_id",
+    header: "البلد",
+  },
+];
+function uploadImage(val) {
+  url.value = val;
 }
-getAllBanners()
-
-function uploadImage (val) {
-  bannerService.createBanner({imageUrl: val}).then(()=>{
-    getAllBanners()
-  })
+const actions = [
+  {
+    text: "مسح",
+    icon: "pi pi-trash",
+    action: (val) =>
+      window.showDeleteModal({
+        title: "تواصل",
+        id: val.id,
+        url: "admin/banner",
+      }),
+  },
+];
+function onsubmit(data) {
+  console.log(details.value);
+  const payload = {
+    ...data,
+    imageUrl: url.value,
+  };
+  console.log(payload);
+  bannerServices.createBanner(payload).then((res) => {
+    console.log(res);
+  });
 }
-function deleteImage(id) {
-  bannerService.deleteBanner(id).then(()=>{
-    getAllBanners()
-  })
-}
+const details = ref({});
+const visible = ref(false);
+const url = ref("");
+const filter = ref({ title: null, country_id: null });
+const edit = ref(false);
 </script>
